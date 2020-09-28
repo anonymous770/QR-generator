1cbee0428694a498162d1dc9f0c58928326272e4
anonymous <64162091+anonymous770@users.noreply.github.com>
2020-09-28 20:42:47 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,12 @@
-#Author :- AslamMiya
 import pyqrcode
 import os, glob
 import sys
-
-dir_name = ('/sdcard/DCIM/QR codes')
-if not os.path.exists(dir_name):
-  try:
-    os.mkdir('/sdcard/DCIM/QR codes')
-  except FileExistsError:
-    pass
+    
+  # Creating png directory
+  dir_name = ('/sdcard/DCIM/QR codes')
+  if not os.path.exists(dir_name):
+    try:
+      os.mkdir('/sdcard/DCIM/QR codes')
+    except FileExistsError:
+      pass
+  # Finished creating directory
